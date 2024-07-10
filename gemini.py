b5b04b15dabd92d1a042e9d8e584e5d91862f970
Jennifer Lo <66985729+Jelo101@users.noreply.github.com>
2024-07-10 15:23:22 +0800

    Dynamic Template for News Article and Tenancy Agreement

--- gemini.py
+++ gemini.py
@@ -7,32 +7,39 @@ load_dotenv()
 # Gemini API GQA model (previously PaLM 2 API)
 genai.configure(api_key=os.getenv('MAKERSUITE_API_KEY'))
 
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
 generation_config = {
-  "temperature": 0.8,
-  "top_p": 1,
+  "temperature": 1,
+  "top_p": 0.5,
   "top_k": 1,
   "max_output_tokens": 2048,
+  # "response_mime_type": "text/plain",
 }
 
-safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-]
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
 
-model = genai.GenerativeModel(model_name="gemini-pro",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
+chat_session = model.start_chat(
+  history=[
+  ]
+)
